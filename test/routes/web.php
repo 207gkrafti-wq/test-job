91d30b9a7aea87f319c527f301a477cc222d2bec
EASE <207gkrafti@gmail.com>
2026-07-03 11:09:23 +0300

    отключил кеширование

--- test/routes/web.php
+++ test/routes/web.php
@@ -5,20 +5,23 @@ use App\Http\Controllers\FormController;
 use App\Http\Controllers\VisitLinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/login', [BasicController::class, 'log'])->name('login');
-Route::get('/reg', [BasicController::class, 'reg'])->name('reg');
-Route::post('/log', [FormController::class, 'log'])->name('log.form');
-Route::post('/reg', [FormController::class, 'reg'])->name('reg.form');
-Route::get('/links/del', function () {
-    abort(404);
+Route::middleware(['guest','nocache'])->group(function () {
+    Route::get('/login', [BasicController::class, 'log'])->name('login');
+    Route::get('/reg', [BasicController::class, 'reg'])->name('reg');
+    Route::post('/log', [FormController::class, 'log'])->name('log.form');
+    Route::post('/reg', [FormController::class, 'reg'])->name('reg.form');
+    Route::get('/links/del', function () {
+        abort(404);
+    });
 });
-Route::middleware(['auth'])->group(function () {
+Route::middleware(['auth', 'nocache'])->group(function () {
     Route::get('/', [BasicController::class, 'home'])->name('home');
     Route::get('/links', [BasicController::class, 'links'])->name('links');
     Route::post('/', [FormController::class, 'linkCut'])->name('link.form');
     Route::post('/links/del', [FormController::class, 'linkDel'])->name('link.del');
     Route::get('/link/{id}', [BasicController::class, 'linkOpen'])->name('link.open');
+    Route::get('/logout', [BasicController::class, 'logout'])->name('logout');
 });
 
-Route::get('/logout', [BasicController::class, 'logout'])->name('logout');
+
 Route::get('/{code}', [VisitLinkController::class, 'openLink']);
