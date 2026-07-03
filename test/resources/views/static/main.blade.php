63d46f5c6d967f4f5e0bb26b281a7ebd0fa67967
EASE <207gkrafti@gmail.com>
2026-07-03 11:21:25 +0300

    убран script в main

--- test/resources/views/static/main.blade.php
+++ test/resources/views/static/main.blade.php
@@ -5,17 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script>
-        /**
-         * @type {Event} e
-         */
-        window.addEventListener('pageshow', function(e) {
-            if (e.persisted || (window.performance && window.performance.navigation.type === 2)) {
-                document.querySelector('main').style.display = 'none'
-                window.location.reload()
-            }
-        })
-    </script>
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
